73a2f0963dfae4e9e31d14088248590963f772e6
Ragavi987 <75991043+Ragavi987@users.noreply.github.com>
2021-01-06 22:00:10 -0800

    Update admin.py

--- Hospital Management/admin.py
+++ Hospital Management/admin.py
@@ -5,16 +5,6 @@ from .models import *
 # Register models.
 
 
-@admin.register(Doctor)
-class DoctorAdmin(admin.ModelAdmin):
-    list_display = ('name','mobile_no')
-
-
-@admin.register(Patient)
-class PatientAdmin(admin.ModelAdmin):
-    list_display = ('name','mobile_no','email','address')
-
-
-@admin.register(Visits)
-class VisitsAdmin(admin.ModelAdmin):
-    list_display = ('visit_date','purpose_of_visit','height','weight','temperature','bp','notes')
+admin.site.register(Doctor)
+admin.site.register(Patient_details)
+admin.site.register(Visits_Details)
